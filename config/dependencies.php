3f2d4323fa31cf855ea58212ac3cbbbd53b1e109
Olukai UAT <webuser@uat-olukai.robofirm.net>
2018-11-01 11:57:05 -0500

    CN-44 Removed NoCodeDeploymentState and NoAppMaintenanceStrategy

--- config/dependencies.php
+++ config/dependencies.php
@@ -3,10 +3,6 @@
 namespace ConductorAppOrchestration;
 
 return [
-    'aliases' => [
-        Deploy\CodeDeploymentStateInterface::class => Deploy\NoCodeDeploymentState::class,
-        Maintenance\MaintenanceStrategyInterface::class => Maintenance\NoAppMaintenanceStrategy::class,
-    ],
     'factories' => [
         Config\ApplicationConfig::class => Config\ApplicationConfigFactory::class,
     ],
